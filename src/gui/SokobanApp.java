aeb5ed7c1eab17b0871326aa5863b334c45032ec
Yves Vanduynslager <yves.vanduynslager.j1307@student.hogent.be>
2015-02-15 17:44:10 +0100

    Alternatieve verlopen 1A/3A geïmplementeerd

--- src/gui/SokobanApp.java
+++ src/gui/SokobanApp.java
@@ -4,7 +4,8 @@ import domein.DomeinController;
 import java.util.Scanner;
 
 /**
- * Main-klasse.
+ * Main-klasse. Dit is de klasse die de UI zal starten en weergeven/tekenen
+ *
  * @author Yves
  */
 public class SokobanApp
@@ -14,30 +15,82 @@ public class SokobanApp
         Scanner scanner = new Scanner(System.in);
         DomeinController spelController = new DomeinController();
         String[] spelerGegevens;
-        do
-        {
-            System.out.print("Geef gebruikersnaam: ");
-            String gebruikersnaam = scanner.next();
-            System.out.print("Geef wachtwoord: ");
-            String wachtwoord = scanner.next();
 
-            spelController.meldAan(gebruikersnaam, wachtwoord);
-            spelerGegevens = spelController.getSpeler();
-            
-            if(spelerGegevens[0] == null) /*Bij het uitvoeren van een sql-statement met onbestaande
-                gebruikersnaam of onbestaand wachtwoord, zal de spelerGegevens-string gevuld worden met null-waarden.
-            */
+        System.out.printf("%s%n%s%n%s%n%s%n","Wilt u:", "[1] registreren", "[2] aanmelden", "[0] stoppen");
+        System.out.print("Uw keuze: ");
+        int keuze = scanner.nextInt();
+        while (keuze != 0)
+        {
+            if (keuze == 1)
             {
-                System.out.println("Gebruiker niet gevonden of foutief wachtwoord ingegeven, probeer opnieuw");
+                //use case registreer
+                System.out.print(" ------------");
+                System.out.print("| REGISTREER |");
+                System.out.print(" ------------");
+                System.exit(0);
+            }
+            else
+            {
+                if (keuze == 2)
+                {
+                    System.out.println();
+                    System.out.printf("%s%n%s%n%s%n", " -----------", "| AANMELDEN |", " -----------");
+                    do
+                    {
+                        System.out.print("Geef gebruikersnaam ('stop' om te stoppen): ");
+                        String gebruikersnaam = scanner.next();
+                        if (gebruikersnaam.equals("stop"))
+                        {
+                            System.exit(0);
+                        }
+                        System.out.print("Geef wachtwoord ('stop' om te stoppen): ");
+                        String wachtwoord = scanner.next();
+                        if (wachtwoord.equals("stop"))
+                        {
+                            System.exit(0);
+                        }
+
+                        spelController.meldAan(gebruikersnaam, wachtwoord);
+                        spelerGegevens = spelController.getSpeler();
+
+                        if (spelerGegevens[0] == null) /*Bij het uitvoeren van een sql-statement met onbestaande
+                         gebruikersnaam of onbestaand wachtwoord, zal de spelerGegevens-string gevuld worden met null-waarden.*/
+
+                        {
+                            System.out.println("\nGebruiker niet gevonden of foutief wachtwoord ingegeven, probeer opnieuw");
+                        }
+                    }
+                    while (spelerGegevens[0] == null); /*als er geen gegevens opgehaald werden uit de databank
+                     (dus null, ofwel gebruiker niet gevonden, ofwel wachtwoord niet gevonden) zal de lus
+                     opnieuw doorlopen worden.
+                     */
+
+                    System.out.println();
+                    System.out.println("Succesvol aangemeld met volgende gegevens: ");
+                    System.out.printf("%14s%15s%n", "Gebruikersnaam", "Adminrechten"); //wachtwoord wordt hier niet meer weergegeven.
+                    System.out.printf("%14s%15b%n", spelerGegevens[1], spelController.getSpelerAdminrechten());
+                    System.out.println();
+
+                    System.out.printf("%s%n%s%n%s%n%s%n", "Keuzemenu:", "[1] Speel spel", "[2] Configureer nieuw spel", "[3] Wijzig een bestaand spel");
+                    System.out.print("Uw keuze: ");
+                    keuze = scanner.nextInt();
+                    switch (keuze)
+                    {
+                        /*hier voorlopig nog System.exit(0) om het programma te doen stoppen */
+                        case 1:
+                            System.exit(0); //start use case Speel spel
+                            break;
+                        case 2:
+                            System.exit(0); //start use case Configureer nieuw spel
+                            break;
+                        case 3:
+                            System.exit(0); //start use case Wijzig spel
+                            break;
+                        default:
+                            System.exit(0);
+                    }
+                }
             }
         }
-        while (spelerGegevens[0] == null); //hier nogmaals controle om lus opnieuw te starten
-        
-        System.out.println("Succesvol aangemeld met volgende gegevens: ");
-        System.out.println();
-        System.out.printf("%s%20s%20s%n", "ID", "Gebruikersnaam", "Adminrechten"); //wachtwoord geef ik hier niet meer weer.
-        System.out.printf("%s%20s%20b%n", spelerGegevens[0], spelerGegevens[1],
-                spelController.getSpelerAdminrechten());
-        System.out.println();
     }
 }

93a14e6acd2743778919b79719a5a08db0b63537
Michiel <Michiel@192.168.0.136>
2015-03-08 18:36:23 +0100

    Eerste implementatie van de methode verplaatsMannetje (w.i.p.)

--- src/domein/Spelbord.java
+++ src/domein/Spelbord.java
@@ -4,21 +4,27 @@ import java.util.*;
 
 public class Spelbord {
 
-	Collection<Veld> velden;
+	/*
+        * Over de velden.
+        * De buitenste Arraylist bevat een ArrayList van velden. Zo is het mogelijk om met 
+        * velden.get(y).get(x) aan de hand van coördinaten een Veld op te halen uit de "lijst"
+        * dit was veel moeilijker in een Collection.
+        */
+        ArrayList<ArrayList<Veld>> velden;
 	Mannetje mannetje;
 	private int spelbordID;
 
-    public Spelbord(Collection<Veld> velden, Mannetje mannetje, int spelbordID) {
+    public Spelbord(ArrayList<ArrayList<Veld>> velden, Mannetje mannetje, int spelbordID) {
         this.velden = velden;
         this.mannetje = mannetje;
         this.spelbordID = spelbordID;
     }
 
-    public Collection<Veld> getVelden() {
+    public ArrayList<ArrayList<Veld>> getVelden() {
         return velden;
     }
 
-    public void setVelden(Collection<Veld> velden) {
+    public void setVelden(ArrayList<ArrayList<Veld>> velden) {
         this.velden = velden;
     }
 
@@ -37,5 +43,59 @@ public class Spelbord {
     public void setSpelbordID(int spelbordID) {
         this.spelbordID = spelbordID;
     }
-
+    
+    public void verplaatsMannetje(String richting){
+        int x = mannetje.getxPositie();
+        int y = mannetje.getyPositie();
+        
+        switch (richting){
+            case "up":
+                if (velden.get(y).get(x-1) instanceof Veld && x-1>0){
+                    mannetje.setxPositie(x-1);
+                }
+                else if (velden.get(y).get(x-1) instanceof Kist && x-1>0){
+                    if (velden.get(y).get(x-2) instanceof Veld && x-2>0){
+                        
+                        mannetje.setxPositie(x-1);
+                    }
+                }
+                break;
+            case "down":
+                if (velden.get(y).get(x+1) instanceof Veld && x+1<10){
+                    mannetje.setxPositie(x+1);
+                }
+                else if (velden.get(y).get(x+1) instanceof Kist && x+1<10){
+                    if (velden.get(y).get(x+2) instanceof Veld && x+2<10){
+                        
+                        mannetje.setxPositie(x+1);
+                    }
+                }
+                break;
+            case "left":
+                if (velden.get(y-1).get(x) instanceof Veld && y-1>0){
+                    mannetje.setyPositie(y-1);
+                }
+                else if (velden.get(y-1).get(x) instanceof Kist && y-1>0){
+                    if (velden.get(y-2).get(x) instanceof Veld && y-2>0){
+                        
+                        mannetje.setyPositie(y-1);
+                    }
+                }
+                break;
+            case "right":
+                if (velden.get(y+1).get(x) instanceof Veld && y-1<10){
+                    mannetje.setyPositie(y+1);
+                }
+                else if (velden.get(y+1).get(x) instanceof Kist && y+1<10){
+                    if (velden.get(y+2).get(x) instanceof Veld && y+2<10){
+                        
+                        mannetje.setyPositie(y+1);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        
+    }
 }

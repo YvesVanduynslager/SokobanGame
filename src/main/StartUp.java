d74891f473b51cb06a599a30686ed8c1c5c2a0ee
Michiel <Michiel@192.168.0.136>
2015-03-17 17:18:15 +0100

    Kleine aanpassing in klasse StartUp: het is nu mogelijk om het programma te starten via Run Project i.p.v. steeds de StartUp klasse apart te runnen.

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -1,15 +1,21 @@
 package main;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import domein.DomeinController;
 import gui.ConsoleApplicatie;
+
 
 /**
  * Main-klasse. Dit is de klasse die de UI zal starten en weergeven/tekenen
  *
  * @author Yves
  */
-public class StartUp
+public class StartUp extends Application
 {
+    @Override
+    public void start(Stage primaryStage){}
+    
     public static void main(String args[])
     {
         DomeinController controller = new DomeinController();

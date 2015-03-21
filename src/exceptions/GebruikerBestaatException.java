b1ad18cfc7c8b514656640d69b07ca36759c5f56
Yves Vanduynslager <yves.vanduynslager.j1307@student.hogent.be>
2015-03-21 12:40:24 +0100

    Commentaar in domein-package en exceptions volledig aangevuld.

--- src/exceptions/GebruikerBestaatException.java
+++ src/exceptions/GebruikerBestaatException.java
@@ -1,26 +1,48 @@
 package exceptions;
 
 /**
+ * Klasse die erft van Exception, gebruikt bij registreren als een gebruiker al
+ * bestaat in de databank.
  *
  * @author Yves
  */
 public class GebruikerBestaatException extends Exception
 {
+    /**
+     * Default constructor
+     */
     public GebruikerBestaatException()
     {
-        
     }
-    
+
+    /**
+     * Geeft een bericht mee aan de exception.
+     *
+     * @param message Bericht als String.
+     */
     public GebruikerBestaatException(String message)
     {
         super(message);
     }
-    
+
+    /**
+     * Geeft een bericht en object mee met info over de code die de exception
+     * heeft veroorzaakt.
+     *
+     * @param message Bericht als String.
+     * @param cause object met info over de exception.
+     */
     public GebruikerBestaatException(String message, Throwable cause)
     {
         super(message, cause);
     }
-    
+
+    /**
+     * Geet een object mee met info over de code die de exception heeft
+     * veroorzaakt.
+     *
+     * @param cause object met info over de exception.
+     */
     public GebruikerBestaatException(Throwable cause)
     {
         super(cause);
